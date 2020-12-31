d46041afac77ce67d5c0f94d0d044607bcd98b12
LuisD <luis.domiciano@hotmail.com>
2020-12-31 16:17:09 -0300

    feat: category imc

--- iniciantes/01-Calculadora-IMC/category-imc.js
+++ iniciantes/01-Calculadora-IMC/category-imc.js
@@ -1,28 +1,26 @@
-const imc = require('./calculate-imc')
-
-function isUnderWeight () {
+function isUnderWeight (imc) {
     return imc < 18.5
 }
-function isNormalweight () {
+function isNormalweight (imc) {
     return imc >= 18.5 && imc <= 24.9
 }
-function isOverWeight () {
+function isOverWeight (imc) {
     return imc >= 25 && imc <= 29.9
 }
-function isObesityOne () {
+function isObesityOne (imc) {
     return imc >= 30 && imc <= 34.9
 }
-function isObesityTwo () {
+function isObesityTwo (imc) {
     return imc >= 35 && imc <= 39.9
 }
-function isObesityThree () {
+function isObesityThree (imc) {
     return imc >= 40
 }
-module.exports = function () {
-    if (isUnderWeight()) return `Imc: ${imc} está abaixo do peso`
-    if (isNormalweight()) return `Imc: ${imc} está com peso normal`
-    if (isOverWeight()) return `Imc: ${imc} está acima do peso`
-    if (isObesityOne()) return `Imc: ${imc} obesidade grau 1`
-    if (isObesityTwo()) return `Imc: ${imc} obesidade grau 2`
-    if (isObesityThree()) return `Imc: ${imc} obesidade grau 3`
+module.exports = function (imc) {
+    if (isUnderWeight(imc)) return `Imc: ${imc} está abaixo do peso`
+    if (isNormalweight(imc)) return `Imc: ${imc} está com peso normal`
+    if (isOverWeight(imc)) return `Imc: ${imc} está acima do peso`
+    if (isObesityOne(imc)) return `Imc: ${imc} obesidade grau 1`
+    if (isObesityTwo(imc)) return `Imc: ${imc} obesidade grau 2`
+    if (isObesityThree(imc)) return `Imc: ${imc} obesidade grau 3`
 }
